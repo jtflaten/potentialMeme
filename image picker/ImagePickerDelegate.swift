c6c43a95e8c88d72fc250c3174035b0360303623
jtflaten <jtflaten@gmail.com>
2016-12-29 21:49:07 -0600

    submitted for 3rd time on 12/29/16

--- image picker/ImagePickerDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImagePickerDelegate.swift
-//  image picker
-//
-//  Created by Jake Flaten on 11/11/16.
-//  Copyright © 2016 Break List. All rights reserved.
-//
-
-import Foundation
---
+++ image picker/NewMeme.swift
@@ -0,0 +1,18 @@
+//
+// newMeme.swift
+//  image picker
+//
+//  Created by Jake Flaten on 11/11/16.
+//  Copyright © 2016 Break List. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct NewMeme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage?
+    let finishedImage: UIImage?
+}
+    
